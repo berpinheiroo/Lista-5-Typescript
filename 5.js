e4b63b617634918aac28ac4e2cdbe6c013231c2b
Bernardo Pinheiro <berpinheiroo@gmail.com>
2022-12-09 20:04:04 -0300

    Ex 3 e arrumando alguns detalhes

--- 5.js
+++ 5.js
@@ -15,18 +15,68 @@
 // minúsculas ou maiúsculas de acordo com a seguinte regra: fazer o mínimo de
 // alterações possíveis, se a contagem de minúsculas e maiúsculas for igual deve
 // retornar apenas minúsculas.
-console.log(TransformarPalavra("coDIGo"));
-function TransformarPalavra(palavra) {
-    var maiusculas = /[A-Z]/g;
-    var minusculas = /[a-z]/g;
-    var contadorMa = palavra.match(maiusculas);
-    var contadorMi = palavra.match(minusculas);
-    console.log(contadorMa);
-    console.log(contadorMi);
-    if (contadorMa.length > contadorMi.length) {
-        return palavra.toUpperCase();
+// console.log(TransformarPalavra("coDIGo"))
+// function TransformarPalavra(palavra: string):string {
+//     let maiusculas : RegExp = /[A-Z]/g
+//     let minusculas : RegExp = /[a-z]/g
+//     let contadorMa: string[] | null = palavra.match(maiusculas)
+//     let contadorMi: string[] | null = palavra.match(minusculas)
+//     console.log(contadorMa)
+//     console.log(contadorMi)
+//     if(contadorMa.length > contadorMi.length){
+//         return palavra.toUpperCase()
+//     }
+//     else{
+//         return palavra.toLowerCase()
+//     }
+// }
+// Crie uma função que retorne o nome do vencedor em uma luta entre dois lutadores.
+// Cada lutador ataca o adversário no seu próprio turno, quem derrubar o outro
+// primeiro vence, um jogador é derrubado quando sua vida for <= 0.
+// Cada lutador é um objeto de uma classe Lutador, portanto você deve criá-lo com as
+// seguintes propriedades: Nome: string, Vida: number, Ataque: number.
+// Tanto vida como ataque devem ser valores inteiros maiores que 0 e a função além
+// dos dois lutadores deve receber um terceiro parâmetro com o nome de quem vai
+// começar atacando.
+// DeclararVencedor(new Lutador(“Fulano”, 10, 2), new Lutador (“Ciclano”, 7, 3),
+// “Ciclano)
+// Cliclano ataca Fulano; Fulano agora tem 7 de vida.
+// Fulano ataca Cliclano; Ciclano agora tem 5 de vida.
+// Cliclano ataca Fulano; Fulano agora tem 4 de vida.
+// Fulano ataca Cliclano; Ciclano agora tem 3 de vida.
+// Cliclano ataca Fulano; Fulano agora tem 1 de vida.
+// Fulano ataca Cliclano; Ciclano agora tem 1 de vida.
+// Cliclano ataca Fulano; Fulano agora tem -2 de vida e caiu. Ciclano ganhou!
+var Lutador = /** @class */ (function () {
+    function Lutador(nome, vida, ataque) {
+        this.Nome = nome;
+        this.Vida = vida;
+        this.Ataque = ataque;
+    }
+    return Lutador;
+}());
+var primeiroLutador = new Lutador("Bernardo", 10, 2);
+var segundoLutador = new Lutador("Tite", 10, 1);
+NomeDoVencedor(primeiroLutador, segundoLutador, "Tite");
+function NomeDoVencedor(primeiroLutador, segundoLutador, primeiroAtacar) {
+    var continuar = true;
+    var lutadores = [];
+    if (primeiroLutador.Nome == primeiroAtacar) {
+        lutadores.push(primeiroLutador, segundoLutador);
     }
     else {
-        return palavra.toLowerCase();
+        lutadores.push(segundoLutador, primeiroLutador);
     }
+    do {
+        lutadores[1].Vida = lutadores[1].Vida - lutadores[0].Ataque;
+        console.log(lutadores[1].Nome, "ataca", lutadores[0].Nome, ";", lutadores[0].Nome, "agora tem", lutadores[0].Vida, "de vida.");
+        if (lutadores[1].Vida <= 0) {
+            continuar = false;
+            console.log("O lutador ".concat(lutadores[1].Nome, " ficou com ").concat(lutadores[1].Vida, " de vida"));
+            console.log("O vencedor da luta e: ", lutadores[0].Nome);
+        }
+        else {
+            lutadores.reverse();
+        }
+    } while (continuar);
 }
